2434b6abffa28e1f50e8cc3cc4ce45b8be53238c
diogoo9 <diogo-ti@outlook.com>
2021-06-14 14:25:58 -0300

    avatar added

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { CreateUserController } from "../modules/accounts/useCases/CreateUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/UpdateUserAvatarUseCase/UpdateUserAvatarController";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const usersRoutes = Router();
 
+const uplodaAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const userController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", userController.handler);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uplodaAvatar.single("file"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
